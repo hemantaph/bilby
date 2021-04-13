0f54be787e8780349471883d8fadc4b17b8470b0
hemantaph <hemantaphurailatpam@gmail.com>
2021-04-12 18:22:28 -0700

    13/4

--- center of mass/WaVel.py
+++ center of mass/WaVel.py
@@ -1,9 +1,5 @@
 ### Computes the waveform for moving sources based on GWSurrogate  ###
 
-import argparse
-import sys
-import subprocess
-
 import gwsurrogate
 import numpy as np
 
@@ -15,19 +11,7 @@ from math import pi
 from math import e
 from numpy import conj
 
-
-# Get the arguments of the wave: total mass (in M_sun), mass ratio, luminosity distance (in Mpc), the sky location of the observer (in rad), the magnitude of the velocity (in km/s), the direction of the velocity (in rad), the time array (in s)
-parser = argparse.ArgumentParser()
-parser.add_argument('M')
-parser.add_argument('q')
-parser.add_argument('dis')
-parser.add_argument('the')
-parser.add_argument('phi')
-parser.add_argument('v_mag')
-parser.add_argument('v_the')
-parser.add_argument('v_phi')
-parser.add_argument('times')
-
+import coeff
 
 # Define constants: speed of light (in m/s), gravitational constant (in m^3/(kg*s^2)), solar mass (in kg) and megaparsec (in m)
 c = 2.99e8
@@ -35,149 +19,161 @@ G = 6.67e-11
 M_sun = 1.99e30
 Mpc = 3.09e22
 
-
-# Set the total mass (in kg), mass ratio, luminosity distance in m and spins
-M = float(parser.parse_args().M)*M_sun
-q = float(parser.parse_args().q)
-dis = float(parser.parse_args().dis)*Mpc
-chiA = [0,0,0]
-chiB = [0,0,0]
-
-# Set sky location of observer (in deg)
-the = pi*float(parser.parse_args().the)/180
-phi = pi*float(parser.parse_args().phi)/180
-
-# Set the magnitude (in c) and the direction of the velocity
-v_mag = float(parser.parse_args().v_mag)/c
-v_the = pi*float(parser.parse_args().v_the)/180
-v_phi = pi*float(parser.parse_args().v_phi)/180
-v = np.array([sin(v_the)*cos(v_phi),sin(v_the)*sin(v_phi),cos(v_the)])
-
-# Set the times for evaluation (in s)
-times = np.array(parser.parse_args().times.split()).astype(np.float)
-
-
-# Compute the Doppler shift and the amplitude of the wave
-Dopp = (1+v_mag*(sin(the)*cos(phi)*v[0]+sin(the)*sin(phi)*v[1]+cos(the)*v[2]))/sqrt(1-v_mag**2)
-amp = G*M/(dis*c**2)
-
-# Transform the times for evaluation in natural units and set f_low to zero so that it can in principle compute all times
-for i in range(len(times)):
-	times[i] = c**3*times[i]/(G*M*Dopp)
-
-f_low = 0
-
-
-# Define binomial function and spherical harmonics
-def binomial(n,k):
-	if n < 0 or k < 0 or n-k < 0:
-		return 0
-	else:
-		return factorial(n)/(factorial(k)*factorial(n-k))
-
 s = -2
-def Y(l,m,THE,PHI):
-	hlp = 0
-	for k in range(max(0,m-s),l-s+1):
-		if binomial(l-s,k) != 0 and binomial(l+s,k+s-m) != 0:
-			hlp = hlp + (-1)**(l-k-s+m)*binomial(l-s,k)*binomial(l+s,k+s-m)*cos(THE/2)**(2*k+s-m)*sin(THE/2)**(2*l-2*k-s+m)*e**(1j*m*PHI)
 
-	return sqrt(factorial(l+m)*factorial(l-m)*(2*l+1)/(4*pi*factorial(l+s)*factorial(l-s)))*hlp
+class Fn:
+    def __init__(self, surrogate_, M_, q_, dis_, the_, phi_, v_mag_, v_the_, v_phi_, times_):
+        
+        self.surrogate_ = surrogate_
+        self.M_ = M_*M_sun
+        self.q_ = q_
+        self.dis_ = dis_*Mpc
+        self.the_ = pi*float(the_)/180
+        self.phi_ = pi*float(phi_)/180
+        self.v_mag_ = float(v_mag_)/c
+        self.v_the_ = pi*float(v_the_)/180
+        self.v_phi_ = pi*float(v_phi_)/180
+        self.times_ = times_
+    
+    # Define binomial function and spherical harmonics
+    def binomial_(self, n, k):
+        if n < 0 or k < 0 or n-k < 0:
+            return 0
+        else:
+            return factorial(n)/(factorial(k)*factorial(n-k))
+            
+    def Y_(self, l, m, THE, PHI):
+        binomial = self.binomial_
+        
+        hlp = 0
+        for k in range(max(0,m-s),l-s+1):
+            if binomial(l-s,k) != 0 and binomial(l+s,k+s-m) != 0:
+                hlp = hlp + (-1)**(l-k-s+m)*binomial(l-s,k)*binomial(l+s,k+s-m)*\
+                cos(THE/2)**(2*k+s-m)*sin(THE/2)**(2*l-2*k-s+m)*e**(1j*m*PHI)
+                
+                return sqrt(factorial(l+m)*factorial(l-m)*(2*l+1)/\
+                            (4*pi*factorial(l+s)*factorial(l-s)))*hlp
+            
+    def WaVe(self):
+        sur = self.surrogate_
+        M = self.M_
+        q = self.q_
+        dis = self.dis_
+        the = self.the_
+        phi = self.the_
+        v_mag = self.v_mag_
+        v_the = self.v_the_
+        v_phi = self.v_phi_
+        times = self.times_
+
+        Y = self.Y_
+        binomial = self.binomial_
+        
+        
+        chiA = [0,0,0]
+        chiB = [0,0,0]
+        
+        v = np.array([sin(v_the)*cos(v_phi),sin(v_the)*sin(v_phi),cos(v_the)])
+        
+        times = np.array(times)
+
+        # Compute the Doppler shift and the amplitude of the wave
+        Dopp = (1+v_mag*(sin(the)*cos(phi)*v[0]+sin(the)*sin(phi)*v[1]+ \
+                         cos(the)*v[2]))/sqrt(1-v_mag**2)
+        amp = G*M/(dis*c**2)
+        
+        # Transform the times for evaluation in natural units and set f_low to zero so that it can in principle compute all times
+        for i in range(len(times)):
+            times[i] = c**3*times[i]/(G*M*Dopp)
+
+        f_low = 0
+        
+        # 'Get' the wave form
+        t, h, dyn = sur(q, chiA, chiB, times=times, f_low=f_low)
+        
+        # Compute and write out the spherical harmonics
+        y = np.zeros((6,12), dtype=complex)
+        
+        for l2 in range(2,6):
+            for m in range(-l2,l2+1):
+                y[l2][m] = Y(l2,m,the,phi)
+                
+        #calling the class for coeff
+        coeff_ = coeff.Fn2(v_mag, v)
+        c0, cp, cm = coeff_.Coefficient()
+        
+        # Compute the beamed modes
+        # Create array for beamed modes
+        hb = {}
+        for mi in h:
+            hb[mi] = [0]*len(t)
+            hb[(mi[0],-mi[1])] = [0]*len(t)
+        
+        
+        # Compute the beamed modes over time
+        for mi in hb:
+            for i in range(len(t)):
+                # Compute the contributions of each mode
+                hlp0 = 0
+                for l0 in range(max(2,abs(mi[1])),6):
+                    try:
+                        h[(l0,abs(mi[1]))]
+                    except KeyError:
+                        pass
+                    else:
+                        if mi[1] < 0:
+                            hlp0 = hlp0 + (1/4**l0)*(-1)**l0*conj(h[(l0,abs(mi[1]))][i])*c0[l0][mi[0]][mi[1]]
+                        else:
+                            hlp0 = hlp0 + (1/4**l0)*h[(l0,mi[1])][i]*c0[l0][mi[0]][mi[1]]
+
+                hlpp = 0
+                for lp in range(max(2,abs(mi[1]+1)),6):
+                    try:
+                        h[(lp,abs(mi[1]+1))]
+                    except KeyError:
+                        pass
+                    else:
+                        if mi[1]+1 < 0:
+                            hlpp = hlpp + (1/4**lp)*(-1)**lp*conj(h[(lp,abs(mi[1]+1))][i])*cp[lp][mi[0]][mi[1]]
+                        else:
+                            hlpp = hlpp + (1/4**lp)*h[(lp,mi[1]+1)][i]*cp[lp][mi[0]][mi[1]]
+
+                hlpm = 0
+                for lm in range(max(2,abs(mi[1]-1)),6):
+                    try:
+                        h[(lm,abs(mi[1]-1))]
+                    except KeyError:
+                        pass
+                    else:
+                        if mi[1]-1 < 0:
+                            hlpm = hlpm + (1/4**lm)*(-1)**lm*conj(h[(lm,abs(mi[1]-1))][i])*cp[lp][mi[0]][mi[1]]
+                        else:
+                            hlpm = hlpm + (1/4**lm)*h[(lm,mi[1]-1)][i]*cm[lm][mi[0]][mi[1]]
+
+                # Sum up the contributions to get the exited mode
+                if mi[1] < 0:
+                    hb[mi][i] = (-1)**mi[0]*conj(h[(mi[0],abs(mi[1]))][i]) + (-1)**abs(mi[1])*(1j/4**(mi[0]+1))*(hlp0+hlpp+hlpm)
+                else:
+                    hb[mi][i] = h[mi][i] + (-1)**mi[1]*(1j/4**(mi[0]+1))*(hlp0+hlpp+hlpm)
 
 
-# Load the surrogate model
-sur = gwsurrogate.LoadSurrogate('NRHybSur3dq8')
+        # Define arrays for the waveforms
+        hp = [0]*len(t)
+        hc = [0]*len(t)
+        
+        # Compute polarizations over time
+        for i in range(len(t)):
 
-# 'Get' the wave form
-t, h, dyn = sur(q, chiA, chiB, times=times, f_low=f_low)
+            hlp = 0
+            for mi in hb:
+                hlp = hlp + hb[mi][i]*y[mi[0]][mi[1]]
 
+            hp[i], hc[i] = hlp.real, hlp.imag
 
-# Compute and write out the spherical harmonics
-y = np.zeros((6,12), dtype=complex)
-
-for l2 in range(2,6):
-	for m in range(-l2,l2+1):
-		y[l2][m] = Y(l2,m,the,phi)
-
-
-# Convert 'v' into a string, run 'coeff.py' and import the velocity coefficients
-v_str = ' '.join(map(str,v))
-subprocess.run([sys.executable, "coeff.py", str(v_mag), v_str])
-coeff = np.load('coeff.npy')
-c0, cp, cm = coeff[0], coeff[1], coeff[2]
-			
-
-# Compute the beamed modes
-# Create array for beamed modes
-hb = {}
-for mi in h:
-	hb[mi] = [0]*len(t)
-	hb[(mi[0],-mi[1])] = [0]*len(t)
-
-# Compute the beamed modes over time
-for mi in hb:
-	for i in range(len(t)):
-		# Compute the contributions of each mode
-		hlp0 = 0
-		for l0 in range(max(2,abs(mi[1])),6):
-			try:
-				h[(l0,abs(mi[1]))]
-			except KeyError:
-				pass
-			else:
-				if mi[1] < 0:
-					hlp0 = hlp0 + (1/4**l0)*(-1)**l0*conj(h[(l0,abs(mi[1]))][i])*c0[l0][mi[0]][mi[1]]
-				else:
-					hlp0 = hlp0 + (1/4**l0)*h[(l0,mi[1])][i]*c0[l0][mi[0]][mi[1]]
-				
-		hlpp = 0
-		for lp in range(max(2,abs(mi[1]+1)),6):
-			try:
-				h[(lp,abs(mi[1]+1))]
-			except KeyError:
-				pass
-			else:
-				if mi[1]+1 < 0:
-					hlpp = hlpp + (1/4**lp)*(-1)**lp*conj(h[(lp,abs(mi[1]+1))][i])*cp[lp][mi[0]][mi[1]]
-				else:
-					hlpp = hlpp + (1/4**lp)*h[(lp,mi[1]+1)][i]*cp[lp][mi[0]][mi[1]]
-
-		hlpm = 0
-		for lm in range(max(2,abs(mi[1]-1)),6):
-			try:
-				h[(lm,abs(mi[1]-1))]
-			except KeyError:
-				pass
-			else:
-				if mi[1]-1 < 0:
-					hlpm = hlpm + (1/4**lm)*(-1)**lm*conj(h[(lm,abs(mi[1]-1))][i])*cp[lp][mi[0]][mi[1]]
-				else:
-					hlpm = hlpm + (1/4**lm)*h[(lm,mi[1]-1)][i]*cm[lm][mi[0]][mi[1]]
-		
-		# Sum up the contributions to get the exited mode
-		if mi[1] < 0:
-			hb[mi][i] = (-1)**mi[0]*conj(h[(mi[0],abs(mi[1]))][i]) + (-1)**abs(mi[1])*(1j/4**(mi[0]+1))*(hlp0+hlpp+hlpm)
-		else:
-			hb[mi][i] = h[mi][i] + (-1)**mi[1]*(1j/4**(mi[0]+1))*(hlp0+hlpp+hlpm)
+            # Convert time and amplitude in 'SI' units
+            hp[i], hc[i] = amp*hp[i], amp*hc[i]
 
 
-# Define arrays for the waveforms
-hp = [0]*len(t)
-hc = [0]*len(t)
-
-# Compute polarizations over time
-for i in range(len(t)):
-	
-	hlp = 0
-	for mi in hb:
-		hlp = hlp + hb[mi][i]*y[mi[0]][mi[1]]
-
-	hp[i], hc[i] = hlp.real, hlp.imag
-
-	# Convert time and amplitude in 'SI' units
-	hp[i], hc[i] = amp*hp[i], amp*hc[i]
-
-
-# Write out the waveforms	
-np.save('WaVel',[hp,hc])
+        # Write out the waveforms
+        return(hp,hc)
 
--- center of mass/velbay.py
+++ center of mass/velbay.py
@@ -1,11 +1,22 @@
-""" Bayes analysis for moving sources """
-
-import sys
-import subprocess
-
-import bilby
+import gwsurrogate
 import numpy as np
 
+from math import cos
+from math import sin
+from math import sqrt
+from math import factorial
+from math import pi
+from math import e
+from numpy import conj
+
+import bilby
+import WaVel
+
+# Define constants: speed of light (in m/s), gravitational constant (in m^3/(kg*s^2)), solar mass (in kg) and megaparsec (in m)
+c = 2.99e8
+G = 6.67e-11
+M_sun = 1.99e30
+Mpc = 3.09e22
 
 # Define labels, sampling frequency and times
 outdir = 'outdir'
@@ -15,56 +26,37 @@ duration = 2.0
 geocent_time = 0.0
 start_time = geocent_time - duration + 0.02
 
+# Load the surrogate model
+surrogate = gwsurrogate.LoadSurrogate('NRHybSur3dq8')
 
 # Define the time-domain model
 def moving_bbh(times, mass, ratio, distance, theta, phi, speed, v_the, v_phi):
-	# Transform times into a string
-	times_str = ' '.join(map(str,times))
 
-	# Run 'WaVel.py' and import the time and polarizations
-	subprocess.run([sys.executable, "WaVel.py", str(mass), str(ratio), str(distance), str(theta), str(phi), str(speed), str(v_the), str(v_phi), times_str])
+    arg_ = {'surrogate_':surrogate,'M_':mass, 'q_':ratio, 'dis_':distance, 'the_':theta, 'phi_':phi, 'v_mag_':speed, 'v_the_':v_the, 'v_phi_':v_phi, 'times_':times}
+    fplus = WaVel.Fn(**arg_)
+    h_plus = fplus.WaVe()[0]
+    h_cross = fplus.WaVe()[1]
 
-	# 'Get' the polarizations
-	WaVel = np.load('WaVel.npy')
-
-
-	# Return results
-	return {'plus': WaVel[0], 'cross': WaVel[1]}
-	
+    return {'plus': h_plus, 'cross': h_cross}
 
 # Define parameters to inject
 injection_parameters = dict(mass=50, ratio=6, distance=300, theta=45, phi=45, speed=3000, v_the=0, v_phi=0, ra=0, dec=0, psi=0, geocent_time=geocent_time)
 
-
 # Generate model with waveform_generator
-waveform = bilby.gw.waveform_generator.WaveformGenerator(sampling_frequency=sampling_frequency, duration=duration, time_domain_source_model=moving_bbh, start_time=start_time)  
-
+waveform_generator = bilby.gw.waveform_generator.WaveformGenerator(sampling_frequency=sampling_frequency, duration=duration, time_domain_source_model=moving_bbh, start_time=start_time)
 
 # Inject signal into ifos
 ifos = bilby.gw.detector.InterferometerList(['H1', 'L1', 'V1'])
 ifos.set_strain_data_from_power_spectral_densities(sampling_frequency=sampling_frequency, duration=duration, start_time=start_time)
-ifos.inject_signal(waveform_generator=waveform, parameters=injection_parameters)
-
+ifos.inject_signal(waveform_generator=waveform_generator, parameters=injection_parameters)
 
 # Create priors
 prior = injection_parameters.copy()
 prior['mass'] = bilby.core.prior.Uniform(45, 55, r'$M$', unit='$M_sun$')
-#prior['ratio'] = bilby.core.prior.Uniform(5, 7, r'$q$')
-#prior['distance'] = bilby.core.prior.Uniform(275, 325, r'd', unit='$Mpc$')
-#prior['theta'] = bilby.core.prior.Uniform(40, 50, r'$\theta$', unit='degree')
-#prior['phi'] = bilby.core.prior.Uniform(40, 50, r'$\phi$', unit='degree')
-#prior['speed'] = bilby.core.prior.Uniform(0, 3000, r'$|v|$', unit='$km/s$')
-#prior['v_the'] = bilby.core.prior.Uniform(0, 40, r'$v_\theta$', unit='$degree$')
-#prior['v_phi'] = bilby.core.prior.Uniform(0, 40, r'$v_\phi$', unit='$degree$')
-
+#prior['mass'] = bilby.core.prior.Uniform(45, 55, r'$M$', unit='$M_sun$')
 
 # Define likelihood
-likelihood = bilby.gw.likelihood.GravitationalWaveTransient(interferometers=ifos, waveform_generator=waveform, priors=prior)
-
+likelihood = bilby.gw.likelihood.GravitationalWaveTransient(interferometers=ifos, waveform_generator=waveform_generator, priors=prior)
 
 # Launch sampler
-result = bilby.core.sampler.run_sampler(likelihood, prior, sampler='dynesty', npoints=200, injection_parameters=injection_parameters, outdir=outdir, label=label, dlogz=3, npool=6)
-
-
-# Plot results
-result.plot_corner()
+result = bilby.core.sampler.run_sampler(likelihood, prior, sampler='dynesty', npoints=500, injection_parameters=injection_parameters, outdir=outdir, label=label, dlogz=3, npool=16)
